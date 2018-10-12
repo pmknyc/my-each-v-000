e8484bc8cfb2437a9c53bea7ad3704bf9a8f7766
Paula McKinley <pmknyc@optimum.net>
2018-10-12 13:44:28 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+# accept an argument of an array 
+# use while loop to iterate over array
+# yield each element in array to a block
+# return original array
+
+def my_each(array) # call original array
+  item = array.length #count items in array
+  while  > 0  #run method until items = 0
+    
 end
+
+my_each(array) do |item|
+  puts item
+end
+  
