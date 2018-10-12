d70c5a7d8495025ca018caf92d5d0a017a5bdc94
Paula McKinley <pmknyc@optimum.net>
2018-10-12 14:50:57 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+# accept an argument of an array
+# use while loop to iterate over array
+# yield each element in array to a block
+# return original array
+array = ["mon", "tue", "wed"]
+def my_each(array) # call original array
+  item = 0 #sets counter & index first item in array
+  while item < array.length  #run method until item count = # items in array
+    yield array[item]
+    item += 1
+  end
+
 end
+
+my_each(array) do |item|
+  puts item
+  array
+  end
