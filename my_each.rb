4a94d0d106e9fca40e81d1988bec647d09d94bd5
Paula McKinley <pmknyc@optimum.net>
2018-10-12 14:20:50 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@
 # use while loop to iterate over array
 # yield each element in array to a block
 # return original array
-
+array = ["mon", "tue", "wed"]
 def my_each(array) # call original array
   item = 0 #sets counter & index first item in array
   while item < array.length  #run method until item count = # items in array
